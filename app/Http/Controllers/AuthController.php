2418366166de38fbc6e3a4455ba472e925f691ea
ses10 <sesma@live.com>
2016-10-16 10:57:57 -0400

    initial login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,4 +13,18 @@ class AuthController extends Controller
     {
     	return view('auth.login'); 
     }
+
+    public function handleLogin(Request $requestk)
+    {
+    	$data = $request->only('email', 'password');
+
+    	//attempt login
+    	if(\Auth::attempt($data)){
+    		return 'logged in';
+    		return redirect()->intended(home);
+    	}
+
+    	//return to form if login unsucessful
+    	return back()->withInput();
+    }
 }
