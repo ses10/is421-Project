3fbd7e14666dd155495d14c526cd8b8378bff47c
ses10 <sesma@live.com>
2016-10-16 11:02:57 -0400

    undo message

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,6 +25,6 @@ class AuthController extends Controller
     	}
 
     	//return to form if login unsucessful
-    	return back()->withInput('login failed');
+    	return back()->withInput();
     }
 }
