dc898e642c7af91727c7229ca6a360527cd65ff1
ses10 <sesma@live.com>
2016-10-24 10:44:31 -0400

    added settings for local dev

--- config/database.php
+++ config/database.php
@@ -4,6 +4,7 @@ $url = parse_url(getenv("DATABASE_URL"));
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
+
 $database = substr($url["path"], 1);
 
 return [
@@ -74,11 +75,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $host,
+            'host' => env('DB_HOST', $host),
             'port' => '5432',
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
