c030b4721fcf338563f6dfbe558c0f482f3d36f3
ses10 <sesma@live.com>
2016-10-25 08:23:54 -0400

    add sales table for user

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.master')
 
 @section('content')
-	<h3>Homepage for user: {{ \Auth::user()->firstName . " " . \Auth::user()->lastName }}</h3>
+	<h3>Homepage for user: {{ $firstName . " " . $lastName }}</h3>
+
+	<br>
+
+	<h4>Sales</h4>
+	{{ link_to_route('home', 'New Sale') }}
+
+	<table class="table">
+		<thead>
+			<tr>
+				<th>Sale ID</th>
+				<th>VIN#</th>
+				<th>Year</th>
+				<th>Make</th>
+				<th>Model</th>
+				<th>Sale Status</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($sales as $sale)
+				<tr> 
+					<td>{{ $sale['id'] }}</td>
+					<td>{{ $sale['vin'] }}</td>
+					<td>{{ $sale->car['year'] }}</td>
+					<td>{{ $sale->car['make'] }}</td>
+					<td>{{ $sale->car['model'] }}</td>
+					<td>{{ $sale->car['saleStatus'] }}</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
+
 @endsection
