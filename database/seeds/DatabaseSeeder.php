6cbe1a95c42f17c2724f8f5b67c62c7609663475
ses10 <sesma@live.com>
2016-11-01 15:33:58 -0400

    update seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(UserTableSeeder::class);
+         $this->call(CarTableSeeder::class);
+         $this->call(SaleTableSeeder::class);
+         $this->call(RoleTableSeeder::class);
     }
 }
